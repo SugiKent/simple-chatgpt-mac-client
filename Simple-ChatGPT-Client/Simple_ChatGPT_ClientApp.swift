ace9210e84bf440d2a257e5694d4a4a570ded2fd
SugiKent <ynmk1995@gmail.com>
2023-04-08 16:56:19 +0900

    Initial Commit

---
+++ Simple-ChatGPT-Client/Simple_ChatGPT_ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_ChatGPT_ClientApp.swift
+//  Simple-ChatGPT-Client
+//
+//  Created by Kento Sugita on 2023/04/08.
+//
+
+import SwiftUI
+
+@main
+struct Simple_ChatGPT_ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
